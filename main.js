3a3d6da7cc6cafc1b30006b0b1a745265c03ddbc
alexv <alexvlass01@gmail.com>
2026-06-03 02:20:28 +0300

    fix: broadcast config changes to renderer on all config saves

--- main.js
+++ main.js
@@ -63,6 +63,7 @@ function loadConfig() {
 
 function saveConfig() {
   configMod.save(config, CONFIG_PATH);
+  broadcastConfig();
 }
 
 // ---------------------------------------------------------------------------
